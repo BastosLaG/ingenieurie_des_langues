010266b690e71374a886efd7bd8ace1845d97b92
BastosLaG <bastien.capiaux@gmail.com>
2024-03-04 19:29:29 +0100

    fin Tp determinant

--- tp/tp3/determinants/tp3_determinant_bastien_capiaux_21002589/determinant.py
+++ tp/tp3/determinants/tp3_determinant_bastien_capiaux_21002589/determinant.py
@@ -3,13 +3,14 @@ import re
 import glob
 import os
 
+
 # Téléchargement des données nltk
 nltk.download('punkt')
 
 # Fonction pour segmenter le texte en phrases
 def segmenter_phrases(texte):
     phrases = nltk.sent_tokenize(texte)
-    phrases_propres = [phrase.replace('\n', ' ') for phrase in phrases]
+    phrases_propres = ['<p>'+phrase.replace('\n', ' ')+'</p>' for phrase in phrases]
     return phrases_propres
 
 def determiner_fonction(mot_precedent):
@@ -24,42 +25,25 @@ def determiner_fonction(mot_precedent):
 
 # Fonction pour annoter les fichiers et écrire les résultats annotés dans un nouveau fichier XML
 def annoter_fichiers(filename):
-    occurrences_articles = {}
-    occurrences_pronoms = {}
-
     with open(filename, 'r') as file:
         fichier = file.read()
     phrases = segmenter_phrases(fichier)
           
     output_filename = os.path.join("output", os.path.basename(filename).replace(".txt", "_annotated.txt"))
     with open(output_filename, 'w') as output_file:
-
         for phrase in phrases:
             pattern = r"(\w+)\s+(le|la|l'|les)\s+(\w+)"
             matches = re.findall(pattern, phrase, re.IGNORECASE)
-            
             for match in matches:
+                determinant_res = ''
                 mot_precedent, determinant, mot_suivant = match
                 fonction = determiner_fonction(mot_precedent.lower())
-                
                 if fonction == "Article_Défini":
-                    determinant = "<article_def>" + determinant + "</article_def>"
-                    if determinant in occurrences_articles:
-                        occurrences_articles[determinant.lower()] += 1
-                    else:
-                        occurrences_articles[determinant.lower()] = 1
+                    determinant_res = "<article_def>" + determinant + "</article_def>"
                 elif fonction == "Pronom_Personnel":
-                    determinant = "<pronom>" + determinant + "</pronom>"
-                    if determinant in occurrences_pronoms:
-                        occurrences_pronoms[determinant.lower()] += 1
-                    else:
-                        occurrences_pronoms[determinant.lower()] = 1
-                        
-        for article, occurrence in occurrences_articles.items():
-            output_file.write(f"<Article_def>{article}</Article_def>\n")
-        for pronom, occurrence in occurrences_pronoms.items():
-            output_file.write(f"<Pronom>{pronom}</Pronom>\n")
-    
+                    determinant_res = "<pronom>" + determinant + "</pronom>"
+                phrase = phrase.replace(f"{mot_precedent} {determinant} {mot_suivant}", f"{mot_precedent} {determinant_res} {mot_suivant}")
+            output_file.write(phrase + '\n')
     print(f"Annotations terminées pour {filename}. Résultats enregistrés dans {output_filename}.")
 
 if __name__ == "__main__":
